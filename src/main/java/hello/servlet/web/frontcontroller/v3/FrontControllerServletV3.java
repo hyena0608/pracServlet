c3ec10d376d63ffc6190b41d9fa4f68debf10ff5
hyena0608 <goatsfish@likelion.org>
2022-01-10 16:45:14 +0900

    220110

--- src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
+++ src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -28,6 +28,7 @@ public class FrontControllerServletV3 extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         String requestURI = request.getRequestURI();
 
         ControllerV3 controller = controllerMap.get(requestURI);
